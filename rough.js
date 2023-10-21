e45365fa1264bc087ed0e42021f861502f0d90e3
Amish2417 <anishraj2417>
2023-10-21 20:33:17 +0530

    spotify

---
+++ rough.js
@@ -0,0 +1,10 @@
+
+
+for(let i=1;i<=800;i++){
+    const container = document.querySelector('.container');
+    const dupdiv = document.createElement("div");
+    dupdiv.classList.add("square");
+    container.appendChild(dupdiv);
+
+}
+
